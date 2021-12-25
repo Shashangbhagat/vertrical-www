7ca0a6ba9ec19206c48f53f902e8f9e23684de02
Shashangbhagat <shashangbhagat@gmail.com>
2021-12-25 09:33:36 +0530

    Added Routing and Structure of Project

---
+++ src/components/RecordDetails/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecordDetails () {
+	return (
+		<div>
+				Record Details
+		</div>
+	)
+}
+
+export default RecordDetails;
---
+++ src/components/RecordsList/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RecordsList () {
+	return (
+		<div>
+				Records List
+		</div>
+	)
+}
+
+export default RecordsList;
